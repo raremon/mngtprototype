03f99520557d6e4f78403142048529d1b48f74b9
Anthony Deco <anthonydeco322@gmail.com>
2017-05-29 23:04:40 +0800

    Android - hotfix

--- application/controllers/api/Mobileapp.php
+++ application/controllers/api/Mobileapp.php
@@ -227,14 +227,14 @@ class Mobileapp extends REST_Controller {
 			$query = $this->Owner_Accounts->request_resetpass($data);
 			if($query > 0){
 				// Gets advertiser email from advertiser id
-				$response = $this->Owners->get_email($response);
+				$response = $this->Owners->get_email($query);
 			}
 			// Account not found
 			else{
 				$response = -1;
 			}
 		}
-		}else{
+		else{
 			// If direct controller access
 			$response = -1;
 		}
