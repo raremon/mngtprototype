49e37063ca3c58dbe84026890e0d4c48cd37f1ac
Anthony Deco <anthonydeco322@gmail.com>
2017-05-31 00:13:17 +0800

    Android - quickfix

--- application/controllers/api/Mobileapp.php
+++ application/controllers/api/Mobileapp.php
@@ -269,7 +269,9 @@ class Mobileapp extends REST_Controller
 		$data = $this->post();
 		
 		// Splits $data to three parts
+		$data1['sales_id'] = $data['sales_id'];
 		$data1['ad_duration'] = $data['ad_duration'];
+		$data1['advertiser_id'] = $data['advertiser_id'];
 		$data1['order_status'] = 0;
 		$data1['date_start'] = $data['date_start'];
 		$data1['date_end'] = $data['date_end'];
